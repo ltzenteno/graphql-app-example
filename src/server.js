a890bb05f46da2d74bbcd1c4ccc52e7139e06402
Luis Zenteno <ltzenteno@gmail.com>
2017-12-19 12:49:52 -0600

    first commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import expressGraphQL from 'express-graphql';
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql:true
+}));
+
+app.listen(4000, () => {
+  console.log('Listening :)');
+});
